01af1e493b65e4c3c30865b006ae39abcf122e8e
Haneen-Noushad <haneennoushad@gmail.com>
2022-04-19 22:07:57 +0400

    6th commit

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+//const flag = 1;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCL2pMAOVjFjTyyiC_jhxE1TVasYU8Vn_s",
+  authDomain: "agros-2ad4a.firebaseapp.com",
+  projectId: "agros-2ad4a",
+  storageBucket: "agros-2ad4a.appspot.com",
+  messagingSenderId: "642301956260",
+  appId: "1:642301956260:web:4ffce60e97ddee934a1631"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
-
     messageElement.textContent = message;
     messageElement.classList.remove("form__message--success", "form__message--error");
     messageElement.classList.add(`form__message--${type}`);
@@ -31,25 +49,66 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
-    });
-
-  
-    
+    });  
 
 
-     loginForm.addEventListener("submit", e => {
+    loginForm.addEventListener("submit", e => {
         e.preventDefault();      
-        const username=document.getElementById("user").value;
-        const password=document.getElementById("pass").value;    
+        const login_email=document.getElementById("user").value;
+        const login_password=document.getElementById("pass").value;    
         
-        if((username === 'AGROS') && (password === '2248')){
-            window.open('./weather.html','_self');
-        }
-        else{
-            setFormMessage(loginForm, "error", "Invalid username/password combination");
-        }         
+        signInWithEmailAndPassword(auth, login_email, login_password)
+        .then((userCredential) => {
+        // Signed in 
+            const user = userCredential.user;
+    
+            const dt = new Date();
+             update(ref(database, 'users/' + user.uid),{
+              last_login: dt,
+            })
+    
+             alert('User logged in successfully!');
+             window.open('./weather.html','_self');
+            // ...
+        })
+       .catch((error) => {
+           const errorCode = error.code;
+           const errorMessage = error.message;
+
+           //alert(errorMessage);
+           setFormMessage(loginForm, "error", "Invalid username/password combination");
+        });
+          
         
     }); 
+
+    
+
+    createAccountForm.addEventListener("submit",e => {
+        e.preventDefault();     
+        const reg_email=document.getElementById("register_email").value;
+        const reg_pass=document.getElementById("register_pass").value;
+        const reg_user=document.getElementById("signupUsername").value;
+        createUserWithEmailAndPassword(auth, reg_email, reg_pass)
+        .then((userCredential) => {
+         
+          const user = userCredential.user;
+    
+          set(ref(database, 'users/' + user.uid),{
+              username: reg_user,
+              email: reg_email
+          })
+    
+          alert('user created!');
+          
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;    
+          alert(errorMessage);        
+        });            
+    });
+
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
@@ -62,7 +121,15 @@ document.addEventListener("DOMContentLoaded", () => {
         inputElement.addEventListener("input", e => {
             e.preventDefault();
             clearInputError(inputElement);
+            
         });
+        inputElement.addEventListener("blur",e => {
+            e.preventDefault();  
+            if(e.target.value.length === 0){
+                setInputError(inputElement,"This field is empty");
+            }                    
+        });
+        
     });
 });
 
